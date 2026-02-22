bb58376be937ae1e2e0fb23aeaf8329fad380faa
Juan David gonzalez <juandavidgonzalez@gmail.com>
2026-02-21 20:26:57 -0500

    Cambio de base de datos SUPABASE

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 class Config:
     SECRET_KEY = "supersecretkey123"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:llNmgtzRQYCZuhCnxCkUmcYXkAaDIOoO@nozomi.proxy.rlwy.net:45884/railway"
+
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.sayoktcxabluuvhbwzkf:1110362741Juan@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
